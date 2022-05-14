38e518d63a1ed47b0c35a1099bbf677dc872c6c0
Tahseen <tahseenislam@hotmail.com>
2022-05-15 02:47:59 +1000

    added firestore database storage

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBM0pLjeHaC-qo1lGZZhghaTO4wy7D6ekE",
   authDomain: "fir-test-2a59e.firebaseapp.com",
@@ -14,3 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app)
+
+export const db = getFirestore(app)
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { FirebaseContextProvider } from './context/FirebaseContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <FirebaseContextProvider>
-      <App />
-    </FirebaseContextProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <FirebaseContextProvider>
+    <App />
+  </FirebaseContextProvider>
+  // </React.StrictMode>
 );
