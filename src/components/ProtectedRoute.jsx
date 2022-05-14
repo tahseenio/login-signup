8bfca8d77280cf86d74a36bef3c9e4fb10559520
Tahseen <tahseenislam@hotmail.com>
2022-05-14 15:55:14 +1000

    refactored protectedroute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,8 +1,11 @@
-import { Navigate } from 'react-router-dom'
-import { useFirebaseContext } from '../context/FirebaseContext'
+import { Navigate } from 'react-router-dom';
+import { useFirebaseContext } from '../context/FirebaseContext';
 
 export const ProtectedRoute = ({ children }) => {
-  const { user, isLoading } = useFirebaseContext()
+  const { user } = useFirebaseContext();
   // !user -> means if the user doesnt exist do something
-  return (isLoading ? "LOADING" : (!user) ? <Navigate to={'/login'} /> : children)
-}
+  return (
+    !user
+      ? <Navigate to={'/login'} />
+      : children);
+};
