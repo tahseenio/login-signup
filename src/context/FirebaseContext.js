0be3b081f83a1c5cc6ba1297ea20c0a8d38a98df
Tahseen <tahseenislam@hotmail.com>
2022-05-14 15:55:14 +1000

    refactored protectedroute

--- src/context/FirebaseContext.js
+++ src/context/FirebaseContext.js
@@ -73,7 +73,7 @@ export const FirebaseContextProvider = ({ children }) => {
         isLoading
       }}
     >
-      {children}
+      {!isLoading ? children : null}
     </FirebaseContext.Provider>
   );
 };
