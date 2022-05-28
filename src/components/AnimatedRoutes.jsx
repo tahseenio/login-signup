cfb9bcaa66aaa480f989505d7d8ca1ecf3c4d7cc
Tahseen <tahseenislam@hotmail.com>
2022-05-29 01:15:07 +1000

    fixed: not including animatedroutes in recent commit

---
+++ src/components/AnimatedRoutes.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+
+import { Home } from '../pages/Home';
+import { Login } from './Login';
+import { Register } from './Register';
+import { ProtectedRoute } from './ProtectedRoute';
+import { ForgotPassword } from './ForgotPassword';
+import { AnimatePresence } from 'framer-motion';
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence exitBeforeEnter={true}>
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot' element={<ForgotPassword />} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
+
+export default AnimatedRoutes
