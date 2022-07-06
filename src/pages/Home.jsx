b6f65c484427a4340a3c80f924c8991454247df3
Tahseen <tahseenislam@hotmail.com>
2022-07-07 05:34:42 +1000

    made chart responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { doc, setDoc, getDoc } from 'firebase/firestore'
+import { doc, setDoc, getDoc } from 'firebase/firestore';
 
 import { useFirebaseContext } from '../context/FirebaseContext';
 import 'chart.js/auto';
@@ -7,13 +7,13 @@ import { Doughnut } from 'react-chartjs-2';
 
 export const Home = () => {
   const { user, handleSignout, db } = useFirebaseContext();
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [colorValues, setColorValues] = useState([])
-  const [redValue, setRedValue] = useState(0)
-  const [blueValue, setBlueValue] = useState(0)
-  const [yellowValue, setYellowValue] = useState(0)
-  const [greenValue, setGreenValue] = useState(0)
+  const [colorValues, setColorValues] = useState([]);
+  const [redValue, setRedValue] = useState(0);
+  const [blueValue, setBlueValue] = useState(0);
+  const [yellowValue, setYellowValue] = useState(0);
+  const [greenValue, setGreenValue] = useState(0);
 
   const initColorDatabase = async () => {
     try {
@@ -21,90 +21,94 @@ export const Home = () => {
         red: 50,
         blue: 30,
         green: 100,
-        yellow: 20
-      }
-      await setDoc(doc(db, 'users', `${user.email}`), userNewColors)
-      console.log("set new doc for initialisation")
+        yellow: 20,
+      };
+      await setDoc(doc(db, 'users', `${user.email}`), userNewColors);
+      console.log('set new doc for initialisation');
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const updateColorDatabase = async (arr) => {
     try {
-      const userNewColors = { ...arr }
-      await setDoc(doc(db, 'users', `${user.email}`), userNewColors, { merge: true })
-      initDatabase()
-      console.log("set new doc for initialisation")
+      const userNewColors = { ...arr };
+      await setDoc(doc(db, 'users', `${user.email}`), userNewColors, {
+        merge: true,
+      });
+      initDatabase();
+      console.log('set new doc for initialisation');
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  const userColorRef = doc(db, 'users', `${user.email}`)
+  const userColorRef = doc(db, 'users', `${user.email}`);
 
   const initDatabase = async () => {
-    const docSnap = await getDoc(userColorRef)
+    const docSnap = await getDoc(userColorRef);
 
     if (docSnap.exists()) {
-      console.log("DOC EXISTS")
-      const docSnapdata = docSnap.data()
-      setColorValues([docSnapdata.red, docSnapdata.blue, docSnapdata.yellow, docSnapdata.green])
-      setRedValue(docSnapdata.red)
-      setBlueValue(docSnapdata.blue)
-      setYellowValue(docSnapdata.yellow)
-      setGreenValue(docSnapdata.green)
-      setIsLoading(false)
+      console.log('DOC EXISTS');
+      const docSnapdata = docSnap.data();
+      setColorValues([
+        docSnapdata.red,
+        docSnapdata.blue,
+        docSnapdata.yellow,
+        docSnapdata.green,
+      ]);
+      setRedValue(docSnapdata.red);
+      setBlueValue(docSnapdata.blue);
+      setYellowValue(docSnapdata.yellow);
+      setGreenValue(docSnapdata.green);
+      setIsLoading(false);
     } else {
-      console.log("NO DOC EXISTS")
-      initColorDatabase()
-      initDatabase()
+      console.log('NO DOC EXISTS');
+      initColorDatabase();
+      initDatabase();
     }
-  }
+  };
 
   useEffect(() => {
-    initDatabase()
+    initDatabase();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   const handleColorChange = (e, color) => {
-    if (typeof e.target.value === 'number') console.log('A NUMER')
-    if (color === 'red') setRedValue(e.target.value)
-    if (color === 'blue') setBlueValue(e.target.value)
-    if (color === 'yellow') setYellowValue(e.target.value)
-    if (color === 'green') setGreenValue(e.target.value)
-  }
+    if (typeof e.target.value === 'number') console.log('A NUMER');
+    if (color === 'red') setRedValue(e.target.value);
+    if (color === 'blue') setBlueValue(e.target.value);
+    if (color === 'yellow') setYellowValue(e.target.value);
+    if (color === 'green') setGreenValue(e.target.value);
+  };
 
   const handleColorSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const newColors = {
       red: Number(redValue),
       blue: Number(blueValue),
       green: Number(greenValue),
-      yellow: Number(yellowValue)
-    }
-    setIsLoading(true)
-    updateColorDatabase(newColors)
-  }
+      yellow: Number(yellowValue),
+    };
+    setIsLoading(true);
+    updateColorDatabase(newColors);
+  };
 
   const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow',
-      'Green'
+    labels: ['Red', 'Blue', 'Yellow', 'Green'],
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: colorValues,
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)',
+          'rgb(81, 191, 62)',
+        ],
+        hoverOffset: 4,
+      },
     ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: colorValues,
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)',
-        'rgb(81, 191, 62)'
-      ],
-      hoverOffset: 4
-    }]
   };
 
   return (
@@ -112,35 +116,53 @@ export const Home = () => {
       {/* <aside className="side-bar">
 
       </aside> */}
-      <main className="container">
-        <div className="row">
-          <div className="home__container">
-            <div className='testing_purposes'>Hello</div>
-
+      <main className='container'>
+        <div className='row'>
+          <nav>
+            <p>Welcome to the Homepage!</p>
+            <p>Logged in as: {user.email}</p>
+            <button onClick={handleSignout}>Sign Out</button>
+          </nav>
+          <div className='home__container'>
+            <div className='chart__container'>
+              {isLoading ? (
+                <div className='chart__skeleton'>
+                  <div className='chaotic-orbit chaotic-orbit--black loader'></div>
+                </div>
+              ) : (
+                <Doughnut data={data} />
+              )}
+            </div>
+            <form onSubmit={handleColorSubmit}>
+              <p>Red</p>
+              <input
+                type='text'
+                value={redValue}
+                onChange={(e) => handleColorChange(e, 'red')}
+              />
+              <p>Blue</p>
+              <input
+                type='text'
+                value={blueValue}
+                onChange={(e) => handleColorChange(e, 'blue')}
+              />
+              <p>Green</p>
+              <input
+                type='text'
+                value={greenValue}
+                onChange={(e) => handleColorChange(e, 'green')}
+              />
+              <p>Yellow</p>
+              <input
+                type='text'
+                value={yellowValue}
+                onChange={(e) => handleColorChange(e, 'yellow')}
+              />
+              <button>Change Values</button>
+            </form>
           </div>
         </div>
       </main>
-      <nav>
-        <p>Welcome to Home</p>
-        <p>Logged in as: {user.email}</p>
-        <button onClick={handleSignout}>Sign Out</button>
-      </nav>
-      <section className=''>
-        <div className="chart__container">
-          {isLoading ? <div className='chart__skeleton'><div className="chaotic-orbit chaotic-orbit--black loader"></div></div> : <Doughnut data={data} />}
-        </div>
-        <form onSubmit={handleColorSubmit}>
-          <p>Red</p>
-          <input type="text" value={redValue} onChange={(e) => handleColorChange(e, 'red')} />
-          <p>Blue</p>
-          <input type="text" value={blueValue} onChange={(e) => handleColorChange(e, 'blue')} />
-          <p>Green</p>
-          <input type="text" value={greenValue} onChange={(e) => handleColorChange(e, 'green')} />
-          <p>Yellow</p>
-          <input type="text" value={yellowValue} onChange={(e) => handleColorChange(e, 'yellow')} />
-          <button>Change Values</button>
-        </form>
-      </section>
     </>
   );
 };
